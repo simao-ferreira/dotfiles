cc413a140f21837dd4fc4226c35e6fd0de495b6b
Simao Ferreira <simaohsferreira@gmail.com>
2019-09-05 01:23:18 +0200

    REVISION | TPi3: Add polybar theme

---
+++ polybar/.config/polybar/simple-amazonia/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar top -c ~/repositories/unix/i3/.config/polybar/config.ini &
+#polybar bottom -c ~/repositories/unix/i3/.config/polybar/config.ini &
+polybar top -c ~/.config/polybar/simple-amazonia/config.ini &
+polybar bottom -c ~/.config/polybar/simple-amazonia/config.ini &
